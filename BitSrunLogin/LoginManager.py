6c2afbe53d3d3c83a6a76467a0364d51fde04e43
Peng Liu <littlenewton6@gmail.com>
2024-08-31 01:47:40 +0800

    [*] Update indention format.

--- BitSrunLogin/LoginManager.py
+++ BitSrunLogin/LoginManager.py
@@ -10,45 +10,47 @@ from .encryption.srun_sha1 import *
 from .encryption.srun_base64 import *
 from .encryption.srun_xencode import *
 
-header={
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36'
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36'
 }
 
-class LoginManager:
-	def __init__(self,
-		url_login_page = "https://portal.ucas.ac.cn/srun_portal_pc?ac_id=1&theme=pro",
-		url_get_challenge_api = "https://portal.ucas.ac.cn/cgi-bin/get_challenge",
-		url_login_api = "https://portal.ucas.ac.cn/cgi-bin/srun_portal",
-		n = "200",
-		vtype = "1",
-		acid = "1",
-		enc = "srun_bx1"
-	):
-		# urls
-		self.url_login_page = url_login_page
-		self.url_get_challenge_api = url_get_challenge_api
-		self.url_login_api = url_login_api
-		
-		# other static parameters
-		self.n = n
-		self.vtype = vtype
-		self.ac_id = acid
-		self.enc = enc
-	
-	def logout(self, ip=""):
-		if ip == "":
-			self.get_ip()
-		else:
-			self.ip = ip
-		logout_info_params = {
-			"callback": "jsonp1583251661367", # This value can be any string, but cannot be absent
-			"action": "logout",
-			"ip": self.ip
-		}
-		
-		self._challenge_response = requests.get(self.url_login_api, params=logout_info_params, headers=header)
-		print(self._challenge_response.text)
 
+class LoginManager:
+    def __init__(self,
+                 url_login_page="https://portal.ucas.ac.cn/srun_portal_pc?ac_id=1&theme=pro",
+                 url_get_challenge_api="https://portal.ucas.ac.cn/cgi-bin/get_challenge",
+                 url_login_api="https://portal.ucas.ac.cn/cgi-bin/srun_portal",
+                 n="200",
+                 vtype="1",
+                 acid="1",
+                 enc="srun_bx1"
+                 ):
+        # urls
+        self.url_login_page = url_login_page
+        self.url_get_challenge_api = url_get_challenge_api
+        self.url_login_api = url_login_api
+
+        # other static parameters
+        self.n = n
+        self.vtype = vtype
+        self.ac_id = acid
+        self.enc = enc
+
+    def logout(self, ip=""):
+        if ip == "":
+            self.get_ip()
+        else:
+            self.ip = ip
+        logout_info_params = {
+            # This value can be any string, but cannot be absent
+            "callback": "jsonp1583251661367",
+            "action": "logout",
+            "ip": self.ip
+        }
+
+        self._challenge_response = requests.get(
+            self.url_login_api, params=logout_info_params, headers=header)
+        print(self._challenge_response.text)
 
     def login(self, username, password, ip=""):
         self.username = username
@@ -89,57 +91,61 @@ class LoginManager:
         return varname in allvars
 
     @infomanage(
-        callinfo = "Getting login page",
-        successinfo = "Successfully get login page",
-        errorinfo = "Failed to get login page, maybe the login page url is not correct"
+        callinfo="Getting login page",
+        successinfo="Successfully get login page",
+        errorinfo="Failed to get login page, maybe the login page url is not correct"
     )
     def _get_login_page(self):
         # Step1: Get login page
         self._page_response = requests.get(self.url_login_page, headers=header)
 
     @checkvars(
-        varlist = "_page_response",
-        errorinfo = "Lack of login page html. Need to run '_get_login_page' in advance to get it"
+        varlist="_page_response",
+        errorinfo="Lack of login page html. Need to run '_get_login_page' in advance to get it"
     )
     @infomanage(
-        callinfo = "Resolving IP from login page html",
-        successinfo = "Successfully resolve IP",
-        errorinfo = "Failed to resolve IP"
+        callinfo="Resolving IP from login page html",
+        successinfo="Successfully resolve IP",
+        errorinfo="Failed to resolve IP"
     )
     def _resolve_ip_from_login_page(self):
-        self.ip = re.search('ip     : "(.*?)"', self._page_response.text).group(1)
+        self.ip = re.search('ip     : "(.*?)"',
+                            self._page_response.text).group(1)
 
     @checkip
     @infomanage(
-        callinfo = "Begin getting challenge",
-        successinfo = "Challenge response successfully received",
-        errorinfo = "Failed to get challenge response, maybe the url_get_challenge_api is not correct." \
-            "Else check params_get_challenge"
+        callinfo="Begin getting challenge",
+        successinfo="Challenge response successfully received",
+        errorinfo="Failed to get challenge response, maybe the url_get_challenge_api is not correct."
+        "Else check params_get_challenge"
     )
     def _get_challenge(self):
         """
         The 'get_challenge' request aims to ask the server to generate a token
         """
         params_get_challenge = {
-            "callback": "jsonp1583251661367", # This value can be any string, but cannot be absent
+            # This value can be any string, but cannot be absent
+            "callback": "jsonp1583251661367",
             "username": self.username,
             "ip": self.ip
         }
 
-        self._challenge_response = requests.get(self.url_get_challenge_api, params=params_get_challenge, headers=header)
+        self._challenge_response = requests.get(
+            self.url_get_challenge_api, params=params_get_challenge, headers=header)
 
     @checkvars(
-        varlist = "_challenge_response",
-        errorinfo = "Lack of challenge response. Need to run '_get_challenge' in advance"
+        varlist="_challenge_response",
+        errorinfo="Lack of challenge response. Need to run '_get_challenge' in advance"
     )
     @infomanage(
-        callinfo = "Resolving token from challenge response",
-        successinfo = "Successfully resolve token",
-        errorinfo = "Failed to resolve token"
+        callinfo="Resolving token from challenge response",
+        successinfo="Successfully resolve token",
+        errorinfo="Failed to resolve token"
     )
     def _resolve_token_from_challenge_response(self):
-        self.token = re.search('"challenge":"(.*?)"', self._challenge_response.text).group(1)
-    
+        self.token = re.search('"challenge":"(.*?)"',
+                               self._challenge_response.text).group(1)
+
     @checkip
     def _generate_info(self):
         info_params = {
@@ -149,18 +155,19 @@ class LoginManager:
             "acid": self.ac_id,
             "enc_ver": self.enc
         }
-        info = re.sub("'",'"',str(info_params))
-        self.info = re.sub(" ",'',info)
+        info = re.sub("'", '"', str(info_params))
+        self.info = re.sub(" ", '', info)
 
     @checkinfo
     @checktoken
     def _encrypt_info(self):
-        self.encrypted_info = "{SRBX1}" + get_base64(get_xencode(self.info, self.token))
+        self.encrypted_info = "{SRBX1}" + \
+            get_base64(get_xencode(self.info, self.token))
 
     @checktoken
     def _generate_md5(self):
         self.md5 = get_md5("", self.token)
-    
+
     @checkmd5
     def _encrypt_md5(self):
         self.encrypted_md5 = "{MD5}" + self.md5
@@ -195,14 +202,15 @@ class LoginManager:
     @checkencryptedinfo
     @checkencryptedchkstr
     @infomanage(
-        callinfo = "Begin to send login info",
-        successinfo = "Login info send successfully",
-        errorinfo = "Failed to send login info"
+        callinfo="Begin to send login info",
+        successinfo="Login info send successfully",
+        errorinfo="Failed to send login info"
     )
     def _send_login_info(self):
         login_info_params = {
-            'callback': 'jsonp1583251661368', # This value can be any string, but cannot be absent
-            'action':'login',
+            # This value can be any string, but cannot be absent
+            'callback': 'jsonp1583251661368',
+            'action': 'login',
             'username': self.username,
             'password': self.encrypted_md5,
             'ac_id': self.ac_id,
@@ -212,16 +220,18 @@ class LoginManager:
             'n': self.n,
             'type': self.vtype
         }
-        self._login_responce = requests.get(self.url_login_api, params=login_info_params, headers=header)
-    
+        self._login_responce = requests.get(
+            self.url_login_api, params=login_info_params, headers=header)
+
     @checkvars(
-        varlist = "_login_responce",
-        errorinfo = "Need _login_responce. Run _send_login_info in advance"
+        varlist="_login_responce",
+        errorinfo="Need _login_responce. Run _send_login_info in advance"
     )
     @infomanage(
-        callinfo = "Resolving login result",
-        successinfo = "Login result successfully resolved",
-        errorinfo = "Cannot resolve login result. Maybe the srun response format is changed"
+        callinfo="Resolving login result",
+        successinfo="Login result successfully resolved",
+        errorinfo="Cannot resolve login result. Maybe the srun response format is changed"
     )
     def _resolve_login_responce(self):
-        self._login_result = re.search('"suc_msg":"(.*?)"', self._login_responce.text).group(1)
+        self._login_result = re.search(
+            '"suc_msg":"(.*?)"', self._login_responce.text).group(1)
